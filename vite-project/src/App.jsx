2f8727482c74c665d1f0240e31a464c82a3c20e2
D.Lakshmi Narasimha <narasimha34327@gmail.com>
2025-04-10 21:17:08 +0530

    folder structure added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-800">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
